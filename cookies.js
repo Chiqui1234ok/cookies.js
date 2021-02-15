b24e15e4b453fe995c2ae7410c7b5f1a3a3864cb
Santiago Gimenez <42920751+Chiqui1234@users.noreply.github.com>
2021-02-15 00:03:49 -0300

    now return FALSE, not NULL
    If getCookie() doesn't find the cookie, returns FALSE

--- cookies.js
+++ cookies.js
@@ -1,12 +1,13 @@
-function setCookie(name,value,days) {
+function saveCookie(name,value,days) {
     var expires = "";
     if (days) {
         var date = new Date();
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/"; /* Uso el ';' cómo parámetro de corte en getCookie() */
+    document.cookie = name + "=" + (value.toString() || "")  + expires + "; path=/"; /* Uso el ';' cómo parámetro de corte en getCookie() */
     console.log('Cookie guardada.');
+    return getCookie(name);
 }
 
 function getCookie(name) {
@@ -17,9 +18,10 @@ function getCookie(name) {
         while (c.charAt(0)==' ') c = c.substring(1,c.length);
         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
     }
-    return null;
+    return false;
 }
 
-function eraseCookie(name) {   
+function removeCookie(name) {   
     document.cookie = name+'=; Max-Age=-99999999;';  /* Con una expiración negativa o anterior a la actual, la cookie se borra */
+    return true;
 }
